7120ab7573e1cceb8b3d3dde12f39d3b2e5358de
Nikos Math <nmathioudis@gmail.com>
2020-04-28 21:15:40 +0300

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = require('express-promise-router')();
+
+const UserController = require('../controllers/users.js');
+
+router.route('/signup')
+    .post(UserController.signUp);
+
+router.route('/signin')
+    .post(UserController.signIn);
+
+router.route('/secret')
+    .get(UserController.secret);
+
+module.exports = router;
